4cd9c2e2013f4bec87b88dbf0ed65283108c086f
od86 <178620777+od86@users.noreply.github.com>
2026-07-07 16:47:55 +0100

    Added basic type casting in Idea model

--- app/Models/Idea.php
+++ app/Models/Idea.php
@@ -2,6 +2,8 @@
 
 namespace App\Models;
 
+use App\IdeaStatus;
+use Illuminate\Database\Eloquent\Casts\AsArrayObject;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
@@ -10,6 +12,11 @@ class Idea extends Model
 {
     /** @use HasFactory<\Database\Factories\IdeaFactory> */
     use HasFactory;
+
+    protected $casts = [
+        'links' => AsArrayObject::class,
+        'status' => IdeaStatus::class,
+    ];
 
     public function user(): BelongsTo
     {
