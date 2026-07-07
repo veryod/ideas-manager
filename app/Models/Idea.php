cdbdc8ca07d6197209fbc08cda93a52fb84b5ffe
od86 <178620777+od86@users.noreply.github.com>
2026-07-07 16:53:26 +0100

    Added relationship between idea and step model

--- app/Models/Idea.php
+++ app/Models/Idea.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Casts\AsArrayObject;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Idea extends Model
 {
@@ -21,5 +22,10 @@ class Idea extends Model
     public function user(): BelongsTo
     {
         return $this->belongsTo(User::class);
+    }
+
+    public function steps(): HasMany
+    {
+        return $this->hasMany(Step::class);
     }
 }
