3ea2b15aadf707f29a490f0c631cd73f9b52e145
od86 <178620777+od86@users.noreply.github.com>
2026-07-08 15:08:38 +0100

    Added attributes to Step model

--- app/Models/Step.php
+++ app/Models/Step.php
@@ -11,6 +11,10 @@ class Step extends Model
     /** @use HasFactory<\Database\Factories\StepFactory> */
     use HasFactory;
 
+    protected $attributes = [
+        'completed' => false,
+    ];
+
     public function idea(): BelongsTo
     {
         return $this->belongsTo(Idea::class);
