590bbcdd178ab4d5d53b8315ed2425d2d79f2e95
od86 <178620777+od86@users.noreply.github.com>
2026-07-08 15:10:00 +0100

    Added basic factory for Idea model

--- database/factories/IdeaFactory.php
+++ database/factories/IdeaFactory.php
@@ -3,6 +3,7 @@
 namespace Database\Factories;
 
 use App\Models\Idea;
+use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
@@ -18,7 +19,10 @@ class IdeaFactory extends Factory
     public function definition(): array
     {
         return [
-            //
+            'user_id' => User::factory(),
+            'title' => fake()->sentence(),
+            'description' => fake()->paragraph(),
+            'links' => [fake()->url()],
         ];
     }
 }
