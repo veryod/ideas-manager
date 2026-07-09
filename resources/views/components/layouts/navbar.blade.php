ff8886a023ca292ae4e956a8aa5105700849b42e
od86 <178620777+od86@users.noreply.github.com>
2026-07-09 09:14:48 +0100

    Created navbar page

---
+++ resources/views/components/layouts/navbar.blade.php
@@ -0,0 +1,24 @@
+<nav>
+  <ul class="flex p-8 flex justify-center gap-32">
+    @guest
+      <div class="login-functions flex">
+        <li class="mr-6"><a class="text-blue-500 hover:underline" href="/register">Sign up</a></li>
+        <li class="mr-6"><a class="text-blue-500 hover:underline" href="/login">Sign in</a></li>
+      </div>
+    @endguest
+    @auth
+      <div class="idea-functions flex">
+        <li class="mr-6"><a class="text-blue-500 hover:underline" href="/ideas">Ideas</a></li>
+        <li class="mr-6"><a class="text-blue-500 hover:underline" href="/ideas/new">New</a></li>
+      </div>
+      <div class="account-functions flex">
+        <li class="mr-6"><a class="text-blue-500 hover:underline" href="/users/edit">Account</a></li>
+        <form action="/signout" method="post">
+          @csrf
+          @method('DELETE')
+          <button class="text-red-500 hover:underline hover:cursor-pointer">Sign out</button>
+        </form>
+      </div>
+    @endauth
+  </ul>
+</nav>
