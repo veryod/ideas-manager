c0c33f2b6b192e4fa3b97a27559c7c199e860a2f
od86 <178620777+od86@users.noreply.github.com>
2026-07-09 09:13:11 +0100

    Added basic test routes to view login/register page

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/register', function () { return view('auth/register'); });
+Route::get('/login', function () { return view('auth/login'); });
