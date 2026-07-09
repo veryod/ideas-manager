a39e8b31c16d7e6edec34aa69578a13c500724fe
veryod <178620777+veryod@users.noreply.github.com>
2026-07-09 22:00:47 +0100

    Added errors to field componenet

--- resources/views/components/auth/field.blade.php
+++ resources/views/components/auth/field.blade.php
@@ -2,5 +2,8 @@
 
 <div class="field">
     <label for="{{ $name }}">{{ $label }}</label>
-    <input type="{{ $type }}" id="{{ $name }}" name="{{ $name }}" {{ $attributes }}>
+    <input type="{{ $type }}" id="{{ $name }}" name="{{ $name }}" value="{{ old($name) }}" {{ $attributes }}>
+    @error($name)
+        <div class="error flex text-red-500">{{ $message }}</div>
+    @enderror
 </div>
