309f2fbe34b6a94ad32749bbd485322cb4af7659
veryod <178620777+veryod@users.noreply.github.com>
2026-07-09 22:00:02 +0100

    Added validation for login and added logout for sessions controller

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -3,11 +3,12 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class SessionsController extends Controller
 {
     /**
-     * Show the form for creating a new resource.
+     * Show the form for logging in.
      */
     public function create()
     {
@@ -15,10 +16,31 @@ class SessionsController extends Controller
     }
 
     /**
-     * Store a newly created resource in storage.
+     * Sign the user in.
      */
     public function store(Request $request)
     {
-        dd(request()->all());
+        $validated = $request->validate([
+            'email' => ['required', 'string', 'email', 'max:255'],
+            'password' => ['required', 'string', 'min:8', 'max:255'],
+        ]);
+
+        if (!Auth::attempt($validated)) {
+            return back()->withErrors(['password', 'Something is wrong with your password dawg.'])->withInput();
+        }
+
+        $request->session()->regenerate();
+
+        return redirect()->intended('/')->with('success', 'Ayee, successful loging cuh.');
+    }
+
+    /**
+     * Sign out the user.
+     */
+    public function destroy()
+    {
+        Auth::logout();
+
+        return redirect('/login');
     }
 }
