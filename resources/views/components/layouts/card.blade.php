2ecfc0a9961de4315b5e30c8e0e3f272c76394d3
veryod <178620777+veryod@users.noreply.github.com>
2026-07-14 08:29:15 +0100

    Added card component for ideas

---
+++ resources/views/components/layouts/card.blade.php
@@ -0,0 +1,15 @@
+@props(['title', 'description', 'image_path', 'status', 'id'])
+
+<a href="/ideas/{{ $id }}">
+    <div class="max-w-sm rounded overflow-hidden shadow-lg bg-gray-900">
+        <img class="w-full" src="{{ $image_path }}">
+        <div class="px-6 py-4">
+            <div class="font-bold text-xl mb-2 text-gray-300">{{ $title }}</div>
+            <p class="text-gray-500 text-base">{{ $description }}</p>
+        </div>
+        <div class="px-6 pt-4 pb-2">
+            <span class="inline-block bg-blue-500 rounded-full px-3 py-1 text-sm font-semibold text-gray-100 mr-2 mb-2">#{{ $status }}</span>
+        </div>
+    </div>
+</a>
+
