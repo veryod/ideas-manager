ef4d22526414e35ece4298750e9ff1d5857d37cd
veryod <178620777+veryod@users.noreply.github.com>
2026-07-15 22:19:58 +0100

    Made addLink script simpler by only adding input elements

--- resources/js/addLinks.js
+++ resources/js/addLinks.js
@@ -1,13 +1,6 @@
-function addListeners() {
+document.querySelector('.add-link-btn').addEventListener('click', () => {
     document.querySelector('.links-input').innerHTML += `
-        <div class="link-input field flex gap-2">
-            <input type="url" name="link" id="link" placeholder="Enter a link here...">
-            <button type="button" class="add-link-btn font-semibold text-3xl text-gray-100 border">+</button>
-        </div>`
-
-    document.querySelectorAll('.add-link-btn').forEach(btn => {
-        btn.addEventListener('click', addListeners);
-    });
-}
-
-addListeners();
+         <div class="link-input field flex gap-2">
+             <input type="url" name="links[]" id="link" placeholder="Enter a link here...">
+         </div>`
+});
