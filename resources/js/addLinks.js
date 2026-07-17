5b91e687b800ce11677abdc6a29b643c7bb33953
veryod <178620777+veryod@users.noreply.github.com>
2026-07-17 22:48:32 +0100

    Fixed query selector name

--- resources/js/addLinks.js
+++ resources/js/addLinks.js
@@ -1,6 +1,6 @@
 document.querySelector('.add-link-btn').addEventListener('click', () => {
-    document.querySelector('.links-input').innerHTML += `
-         <div class="link-input field flex gap-2">
+    document.querySelector('.links-container').innerHTML += `
+         <div class="field flex gap-2">
              <input type="url" name="links[]" id="link" placeholder="Enter a link here...">
          </div>`
 });
