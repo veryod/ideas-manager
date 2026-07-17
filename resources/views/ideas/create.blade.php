5285b990fdea225e49d7656b56266584ed432a34
veryod <178620777+veryod@users.noreply.github.com>
2026-07-17 22:50:27 +0100

    Fixed title input and added steps section to create idea view

--- resources/views/ideas/create.blade.php
+++ resources/views/ideas/create.blade.php
@@ -1,9 +1,15 @@
-@vite(['resources/js/addLinks.js'])
+@vite(['resources/js/addLinks.js', 'resources/js/addSteps.js'])
 <x-layouts.layout>
     <div>
         <form action="/ideas" method="post" class="form w-2xl" autocomplete="off">
             @csrf
-            <x-auth.field name="title" label="Title" type="text" placeholder="Enter a title here..."></x-auth.field>
+            <div class="field">
+                <label for="title">Title</label>
+                <input type="text" name="title" id="title" placeholder="Enter a title here..." required>
+                @error('title')
+                    <div class="error flex text-red-500 mb-4">{{ $message }}</div>
+                @enderror
+            </div>
             <div class="field">
                 <label for="description">Description</label>
                 <textarea name="description" id="description" rows="6" placeholder="Enter a description here..."></textarea>
@@ -19,13 +25,23 @@
                     <option value="completed">Completed</option>
                 </select>
             </div>
-            <div class="links-input">
+
+            <div class="steps-container">
+                <label for="steps">Steps</label>
+            </div>
+            <button class="add-step-btn font-semibold text-2xl text-gray-100 border w-full mb-4" type="button" data-test="add-step-btn">+</button>
+            @error('steps.*')
+                <div class="error flex text-red-500 mb-4">{{ $message }}</div>
+            @enderror
+
+            <div class="links-container">
                 <label for="links">Links</label>
             </div>
             <button class="add-link-btn font-semibold text-2xl text-gray-100 border w-full mb-4" type="button" data-test="add-link-btn">+</button>
             @error('links.*')
                 <div class="error flex text-red-500 mb-4">{{ $message }}</div>
             @enderror
+
             <div class="actions">
                 <button class="form-btn" type="submit" data-test="create-btn">Create</button>
             </div>
