a7ee284963fa98b41b2fa14010befa0cd21f4607
veryod <178620777+veryod@users.noreply.github.com>
2026-07-18 22:11:24 +0100

    Added creation and addition of image path to idea in store function

--- app/Http/Controllers/IdeaController.php
+++ app/Http/Controllers/IdeaController.php
@@ -37,10 +37,15 @@ class IdeaController extends Controller
      */
     public function store(StoreIdeaRequest $request)
     {
-        $idea = Auth::user()->ideas()->create($request->safe()->except('steps'));
+        $idea = Auth::user()->ideas()->create($request->safe()->except(['steps', 'image']));
         $idea->steps()->createMany(
             collect($request->steps)->map(fn ($step) => ['description' => $step])
         );
+        
+        $imagePath = $request->image->store('ideas', 'public');
+        $idea->update([
+            'image_path' => $imagePath,
+        ]);
 
         return redirect("ideas/$idea->id")->with('success', 'Idea created.');
     }
